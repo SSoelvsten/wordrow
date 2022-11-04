7eba16506e098559c76d1c2e09f68405491d1dda
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-11-04 18:19:19 +0100

    Switch to React's stateful functional styling stuff

--- game/src/game.tsx
+++ game/src/game.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GameInstance } from './game-instance';
 import InputBox from './input-box';
 
@@ -6,22 +6,22 @@ export interface GameProps {
     instance: GameInstance,
 }
 
+const shuffle = (chars: string[]) => {
+    return chars.sort(() => Math.random() - 0.5);
+}
+
 const Game = ({ instance: { anagrams } }: GameProps) => {
     const words: number = anagrams.length;
-
-    var chars: string[] = anagrams[words-1].split('');
-
-    // TODO: shuffle until hamming distance is large enough
-    chars.sort(() => Math.random() - 0.5);
+    const [chars, setChars] = useState(shuffle(anagrams[words-1].split('')));
 
     const word_length: number = chars.length;
 
     return (
-        <div>
+        <>
             {chars.map((c) => (<InputBox content={c} />))}
 
             {JSON.stringify(anagrams)}
-        </div>
+        </>
     );
 }
 
