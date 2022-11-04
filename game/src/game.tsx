8391338e749096f34849870cd9c70c930323a939
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-11-04 17:51:30 +0100

    Create input box components from inptu

--- game/src/game.tsx
+++ game/src/game.tsx
@@ -6,13 +6,21 @@ export interface GameProps {
     instance: GameInstance,
 }
 
-const Game = (props: GameProps) => {
+const Game = ({ instance: { anagrams } }: GameProps) => {
+    const words: number = anagrams.length;
+
+    var chars: string[] = anagrams[words-1].split('');
+
+    // TODO: shuffle until hamming distance is large enough
+    chars.sort(() => Math.random() - 0.5);
+
+    const word_length: number = chars.length;
+
     return (
         <div>
-            <InputBox content="a" />
-            <InputBox content="b" />
+            {chars.map((c) => (<InputBox content={c} />))}
 
-            {JSON.stringify(props.instance)}
+            {JSON.stringify(anagrams)}
         </div>
     );
 }
