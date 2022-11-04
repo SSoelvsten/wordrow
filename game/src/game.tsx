71b9ea9914514c3006a58eedfe4a2e0a21422b6c
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-11-04 19:58:01 +0100

    Draft on input

--- game/src/game.tsx
+++ game/src/game.tsx
@@ -6,22 +6,72 @@ export interface GameProps {
     instance: GameInstance,
 }
 
-const shuffle = (chars: string[]) => {
+type CharIdx = [string, number | null];
+
+const shuffle = (chars: CharIdx[]) => {
     return chars.sort(() => Math.random() - 0.5);
 }
 
 const Game = ({ instance: { anagrams } }: GameProps) => {
     const words: number = anagrams.length;
-    const [chars, setChars] = useState<string[]>(shuffle(anagrams[words-1].split('')));
-    const [selected, setSelected] = useState<number[]>([1,4]);
+    const [chars, setChars] = useState<CharIdx[]>(
+        shuffle(anagrams[words-1].split('').map((c) => [c, null]))
+    );
 
     const word_length: number = chars.length;
+    var selected_length: number = word_length;
+    const selected: string[] = chars
+        // Create a copy of the array
+        .map(i => i)
+        // Sort by character, leaving 'null' at the end
+        .sort(([_ca,ia], [_cb,ib]) => {
+            return ia === null && ib === null ? 0
+                 : ia === null ? -1
+                 : ib === null ? 1
+                 : ia < ib ? -1 : 1})
+        // Display character, if selected
+        .map(([c,i]) => {
+            selected_length--;
+            return i == null ? " " : c
+        });
+    ;
+
+    const onKey = (e: React.KeyboardEvent) => {
+        switch (e.key) {
+        case " ": // Shuffle on 'Spacebar'
+            console.error("Shuffle TODO");
+            break;
+
+        case "Backspace": // Remove Last on 'BackSpace'
+            console.error("Delete TODO");
+            break;
+
+        case "Enter": // Attempt to accept word on 'Enter'
+            console.error("Submit TODO");
+            break;
+
+        default: // Move character, if it exists
+            if (chars.filter(([c,i]) => i === null).map(([c,i]) => c).includes(e.key)) {
+                var hasSelected: boolean = false;
+                setChars(chars.map(([c,i]) => {
+                    if (!hasSelected && i === null && c === e.key) {
+                        hasSelected = true;
+                        return [c,selected_length];
+                    } else {
+                        return [c,i];
+                    }
+                }));
+            }
+
+            break;
+        }
+    }
 
     return (
-        <div id='input-boxes'>
-            {selected.map((i,idx) => (<InputBox content={i + chars[i]} />))}
+        <div id='input-boxes' tabIndex={0} onKeyDown={onKey}>
+            {selected.map((c,idx) => (<InputBox content={c} key={idx} />))}
 
-            {chars.map((c,idx) => (<InputBox content={c} />))}
+            {chars.map(([c,i],idx) => (<InputBox content={c} key={idx} />))}
         </div>
     );
 }
