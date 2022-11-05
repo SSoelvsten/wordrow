b90d6818f1265aa0f3860375162013b9ae233185
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-11-05 19:08:00 +0100

    Parse dictionary and populate anatree works! (pretty 'dopa')

--- dict/src/dict.h
+++ dict/src/dict.h
@@ -23,14 +23,6 @@ std::vector<std::string> split(const std::string &s, char delim) {
   return elems;
 }
 
-bool is_clean(const std::string &s)
-{
-  for (const char c : s) {
-    if (c < 'a' || 'z' < c) return false; // <-- TODO: Dansk
-  }
-  return true;
-}
-
 class dict {
 private:
   class aff_rule {
@@ -57,21 +49,72 @@ private:
 
   std::fstream _dict_stream;
 
-  size_t _out_idx = (size_t) -1;
-  std::vector<std::string> _out_strings;
-
   std::unordered_map<char, std::vector<aff_rule>> _rules;
+
+  std::vector<std::string> _out_strings;
+  size_t _out_idx = (size_t) -1;
+
+  const std::regex _is_lower_char;
+
+private:
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief Populates _out_strings with new words to pull and returns `true` if
+  /// succesful. Keep running, until `can_pull` is empty or it returns `true`.
+  //////////////////////////////////////////////////////////////////////////////
+  bool generate_composite_words()
+  {
+    std::string _raw_line;
+    if (!std::getline(_dict_stream, _raw_line)) { exit(-1); };
+
+    std::vector<std::string> _dict_line = split(_raw_line, '/');
+    assert(_dict_line.size() <= 2);
+
+    std::string _base_word = _dict_line[0];
+    if (!regex_match(_base_word, _is_lower_char)) return false;
+
+    _out_strings.clear();
+    _out_strings = { _base_word };
+
+    if (_dict_line.size() == 2) {
+      //std::cout << "_dict_line: " << _dict_line[1] << std::endl;
+      for (const char i : _dict_line[1]) {
+        for (const aff_rule ar : _rules[i]) {
+          if (!regex_match(_base_word, ar.guard)) { continue; }
+          size_t start_idx = ar.ty == aff_rule::PREFIX ? ar.del.size() : 0u;
+          size_t sub_len = _base_word.size() - ar.del.size();
+
+          std::stringstream ss;
+          ss << (ar.ty == aff_rule::PREFIX ? ar.add : "")
+             << _base_word.substr(start_idx, sub_len)
+             << (ar.ty == aff_rule::SUFFIX ? ar.add : "");
+
+          //std::cout << "ar.add: " << ar.add << std::endl;
+
+          _out_strings.push_back(ss.str());
+          break;
+        }
+      }
+    } else {
+      _out_strings = { _base_word };
+    }
+    _out_idx = (size_t) 0;
+    return true;
+  }
 
 public:
   dict(const std::string& dic_file_path, const std::string& aff_file_path)
-    : _dic_file_path(dic_file_path), _aff_file_path(aff_file_path), _dict_stream(dic_file_path)
+    : _dic_file_path(dic_file_path), _aff_file_path(aff_file_path),
+      _dict_stream(dic_file_path),
+      _is_lower_char("[a-z]*")
   {
+    std::stringstream buffer;
+
     // Read in rules from '.aff' file.
     std::fstream _aff_stream(aff_file_path);
     assert(_aff_stream.is_open());
 
-    while(_aff_stream.peek() != EOF) {
-      std::string _aff_line; = aff_stream.getline();
+    std::string _aff_line;
+    while(std::getline(_aff_stream, _aff_line)) {
       if (_aff_line.find("SFX") == -1 && _aff_line.find("PFX") == -1) continue;
 
       std::vector<aff_rule> _rule_set;
@@ -82,55 +125,35 @@ public:
       const char something_weird = _line_args[2][0];
 
       for (size_t i = 0; i < std::stoul(_line_args[3]); ++i) {
-        _aff_line = aff_stream.getline();
+        if (!std::getline(_aff_stream, _aff_line)) { exit(-1); }
         const std::vector<std::string> _line_args = split(_aff_line, ' ');
 
         const std::string deletion = _line_args[2] == "0" ? "" : _line_args[2];
         const std::string addition = _line_args[3];
         const std::string guard_regex = _line_args[4];
 
-        if (!is_clean(addition)) continue;
+        if (!regex_match(addition, _is_lower_char)) continue;
 
         _rule_set.push_back(aff_rule(type, deletion, addition, guard_regex));
       }
 
-      //if (!_rule_set.empty())
+      // if (_rule_set.size() > 0)
       _rules[identifier] = _rule_set;
     }
+
+    do { } while (can_pull() && !generate_composite_words());
   }
 
-private:
-  void generate_composite_words() 
-  {
-    std::vector<std::string> _dict_line = split(_dict_stream.getline(), '/');
-    assert(_dict_line.size() <= 2);
-
-    std::string _base_word = _dict_line[0];
-
-    _out_strings.erase();
-    if (_dict_line.size() == 2) {
-      for (const char i : _dict_line[1]) {
-        for (const aff_rule ar : _rules[i]) {
-          if (!regex_match(_base_word, ar.guard)) continue;
-            int start_idx = ar.ty == aff_rule::PREFIX ? ar.del.size() : 0;
-            int sub_len = _base_word.size() - ar.del.size();
-
-            _out_strings.push_back((ar.ty == aff_rule::SUFFIX ? "" : ar.add) +
-                                    _base_word.substr(start_idx, sub_len) +
-                                    (ar.ty == aff_rule::SUFFIX ? "" : ar.add));
-        }
-      }
-    } else {
-      _out_strings = { _base_word };
-    }
-    _out_idx = (size_t) 0;
-  }
-
+public:
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief ...
+  //////////////////////////////////////////////////////////////////////////////
   bool can_pull()
   {
-    return out_idx < out_strings.size() || _dict_stream.peek() != EOF;
+    return _out_idx < _out_strings.size() || _dict_stream.peek() != EOF;
   }
 
+public:
   //////////////////////////////////////////////////////////////////////////////
   /// \brief ...
   //////////////////////////////////////////////////////////////////////////////
@@ -141,12 +164,10 @@ private:
     if (_out_idx < _out_strings.size()) {
       return _out_strings[_out_idx];
     }
-
-    generate_composite_words();
-
-    return _out_strings(_out_idx);
+    return _out_strings[_out_idx];
   }
 
+public:
   //////////////////////////////////////////////////////////////////////////////
   /// \brief ...
   //////////////////////////////////////////////////////////////////////////////
@@ -158,9 +179,15 @@ private:
       return _out_strings[_out_idx++];
     }
 
-    generate_composite_words();
+    if (_out_strings.size() <= _out_idx)
+      while(can_pull() && !generate_composite_words()) { }
 
-    return _out_strings(_out_idx++);
+    std::string ret = _out_strings[_out_idx++];
+
+    if (_out_strings.size() <= _out_idx)
+      while(can_pull() && !generate_composite_words()) { }
+
+    return ret;
   }
 };
 
--- dict/src/main.cpp
+++ dict/src/main.cpp
@@ -3,22 +3,28 @@
 #include "anatree.h" // <-- TODO: Use <anatree.h> instead
 #include "dict.h"
 
-int main(int argc, char* argv[]) {
-  dict("./en_US.dic", "./en_US.aff");
+constexpr size_t MIN_LENGTH = 3u;
+constexpr size_t MAX_LENGTH = 8u;
 
-  return 0;
+int main(int argc, char* argv[]) {
+  dict d("./en_US.dic", "./en_US.aff");
   anatree a;
 
-  std::cout << "insert..." << std::endl;
-  a.insert("do");
-  a.insert("god");
-  a.insert("gold");
-  a.insert("dog");
-  a.insert("do");
-  a.insert("food");
+  while(d.can_pull()) {
+    std::string w = d.pull();
+
+    if (MIN_LENGTH <= w.size() && w.size() <= MAX_LENGTH) {
+      std::cout << "   " << w << std::endl;
+      a.insert(w);
+    }
+  }
 
   std::cout << "anagrams_of..." << std::endl;
-  for (const std::string w : a.anagrams_of("food")) {
+  const std::unordered_set res_unsorted = a.anagrams_of("parody");
+  std::vector res_sorted(res_unsorted.begin(), res_unsorted.end());
+  std::sort(res_sorted.begin(), res_sorted.end());
+
+  for (const std::string w : res_sorted) {
     std::cout << "  " << w << std::endl;
   }
 }
