2c83092db92b09edf1856e092b5ccabd6ad5103e
Steffan Sølvsten <soelvsten@cs.au.dk>
2022-11-05 11:52:49 +0100

    Set up basic C++ project

---
+++ dict/src/anatree.h
@@ -0,0 +1,6 @@
+#ifndef ANATREE_H
+#define ANATREE_H
+
+// TODO
+
+#endif // ANATREE_H
---
+++ dict/src/main.cpp
@@ -0,0 +1,8 @@
+#include "anatree.h" // <-- TODO: Use <anatree.h> instead
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello World" << std::endl;
+}
+
