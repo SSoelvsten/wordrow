1a982f6ec110339dbd9f7439471724b760d87684
Steffan Sølvsten <201505832@post.au.dk>
2022-11-26 21:21:13 +0100

    Persist choice of dark settings in LocalStorage

--- game/src/app.tsx
+++ game/src/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as faRegular from '@fortawesome/free-regular-svg-icons'
 import * as faSolid from '@fortawesome/free-solid-svg-icons'
@@ -6,10 +6,16 @@ import { GameLanguage } from './game-screen/game-instance';
 import GameSession from './game-screen/game-session';
 import './app.scss';
 
+const LS_KEYS = {
+  DarkMode: "DarkMode"
+}
+
 const App = () => {
   const [darkMode, setDarkMode] = useState<boolean>(
     () => {
       // TODO: consult local storage for state from previous page
+      const ls_res = localStorage.getItem(LS_KEYS.DarkMode);
+      if (ls_res) { return ls_res === "true"; }
   
       // Ask browser, whether it wants to use dark mode
       return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -17,6 +23,12 @@ const App = () => {
   );
 
   const [gameLanguage, setGameLanguage] = useState<GameLanguage>(GameLanguage.DK)
+
+  const updateLocalStorage = () => {
+    localStorage.setItem(LS_KEYS.DarkMode, `${darkMode}`);
+  }
+
+  useEffect(updateLocalStorage, [darkMode]);
 
   return (
       <div className={`App ${darkMode ? "DarkMode" : ""}`}>
